5cf0d1019677e566101f4181220e1a08568e5cec
Ishan-Mulani <89037495+Ishan-Mulani@users.noreply.github.com>
2023-10-27 23:52:35 +0530

    day 6 :  styling in react

---
+++ Props and More/5 Styling in React/Machine.js
@@ -0,0 +1,25 @@
+function getNum() {
+  return Math.floor(Math.random() * 3);
+}
+
+class Machine extends React.Component {
+  render() {
+    const s1 = getNum();
+    const s2 = getNum();
+    const s3 = getNum();
+    let msg = "";
+    if (s1 == s2 && s2 == s3) {
+      msg = <h1 className="win">You Win !!</h1>;
+    } else {
+      msg = <h1 className="loose">You Loose !!</h1>;
+    }
+    return (
+      <div className="Machine">
+        <Slot num={s1} />
+        <Slot num={s2} />
+        <Slot num={s3} />
+        {msg}
+      </div>
+    );
+  }
+}
